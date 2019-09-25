6f9891a78209f83b169452378bcee2cab17182fd
NeilHansen <neil.hansen17@gmail.com>
2019-09-25 19:46:45 -0400

    added basic cutting prototype, added basic cut functionality, added basic score controller

---
+++ SauceNinjaGame/sauceNinja/Assets/SauceWing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauceWing : MonoBehaviour {
+
+public ScoreController SC;
+void Start()
+    {
+        SC = GameObject.FindObjectOfType<ScoreController>();
+    }
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Cut") {
+            Debug.Log("fuck");
+			Destroy (this.gameObject);
+            SC.Score++;
+		}
+	}
+}
