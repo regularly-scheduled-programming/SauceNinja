598ed094f60fbfc925434259e035e8b3d1722abd
huntersgamedev <hunterfraser.gamedev@gmail.com>
2019-09-25 19:54:52 -0400

    The wings are here!

---
+++ SauceNinjaGame/sauceNinja/Assets/Scripts/WingSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WingSpawner : MonoBehaviour
+{
+    [SerializeField]
+private GameObject chickenPrefab;
+[SerializeField]
+
+private float spawnInterval, objectMinX, objectMaxX,objectY;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     InvokeRepeating("spawnObject",this.spawnInterval, this.spawnInterval);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void spawnObject(){
+    GameObject newWing= Instantiate(this.chickenPrefab);
+    newWing.transform.position = new Vector2(Random.Range(this.objectMinX,this.objectMaxX),this.objectY);
+    }
+}
