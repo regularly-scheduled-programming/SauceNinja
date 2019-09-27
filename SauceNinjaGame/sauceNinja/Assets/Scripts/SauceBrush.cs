23ddbc15c9756f7b278c24ea7831a9c3da05ea19
NeilHansen <neil.hansen17@gmail.com>
2019-09-27 02:00:48 -0400

    added sauce choice functionality, better sauce swipe functionality, added touch functionality not tested yet

---
+++ SauceNinjaGame/sauceNinja/Assets/Scripts/SauceBrush.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauceBrush : MonoBehaviour {
+
+	public GameObject SaucePrefab;
+	public float minVelocity = .001f;
+
+	bool isSaucing = false;
+
+	Vector2 previousPos;
+
+	GameObject currentSauceTrail;
+
+	Rigidbody2D myBody;
+	Camera cam;
+	CircleCollider2D circleCollider;
+
+    public bool isUsingMouse;
+
+	void Start ()
+	{
+		cam = Camera.main;
+		myBody = GetComponent<Rigidbody2D>();
+		circleCollider = GetComponent<CircleCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        //MOUSE
+        if(isUsingMouse)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartSaucing();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                StopSaucing();
+            }
+        }
+        else//TOUCHSCREEN
+        {
+             if (Input.touchCount > 0)
+            {
+                StartSaucing();
+            }
+            else if (Input.touchCount == 0)
+            {
+                StopSaucing();
+            }
+        }
+
+		if (isSaucing)
+		{
+			UpdateSauce();
+		}
+
+	}
+
+	void UpdateSauce ()
+	{
+        if(isUsingMouse)//MOUSE
+        {
+            Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            myBody.position = newPosition;
+
+            float velocity = (newPosition - previousPos).magnitude * Time.deltaTime;
+            if (velocity > minVelocity)
+            {
+                circleCollider.enabled = true;
+            } else
+            {
+                circleCollider.enabled = false;
+            }
+
+            previousPos = newPosition;
+        }
+        else//TOUCHSCREEN
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 newPosition = cam.ScreenToWorldPoint(touch.position);
+            myBody.position = newPosition;
+
+            float velocity = (newPosition - previousPos).magnitude * Time.deltaTime;
+            if (velocity > minVelocity)
+            {
+                circleCollider.enabled = true;
+            } else
+            {
+                circleCollider.enabled = false;
+            }
+
+            previousPos = newPosition;
+        }
+	}
+
+	void StartSaucing ()
+	{
+        if(isUsingMouse)//MOUSE
+        {
+            isSaucing = true;
+            currentSauceTrail = Instantiate(SaucePrefab, transform);
+            previousPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            circleCollider.enabled = false;
+        }
+        else//TOUCHSCREEN
+        {
+            Touch touch = Input.GetTouch(0);
+            isSaucing = true;
+            currentSauceTrail = Instantiate(SaucePrefab, transform);
+            previousPos = cam.ScreenToWorldPoint(touch.position);
+            circleCollider.enabled = false;
+        }
+    }
+
+	void StopSaucing ()
+	{
+		isSaucing = false;
+		currentSauceTrail.transform.SetParent(null);
+		Destroy(currentSauceTrail, 2f);
+		circleCollider.enabled = false;
+	}
+
+}
---
+++ SauceNinjaGame/sauceNinja/Assets/Scripts/SauceTypePicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SauceTypePicker : MonoBehaviour
+{
+
+
+    public enum sauceType { NONE , HoneyGarlic, BBQ, Buffalo}//1 = HG, 2 = BBQ , 3 = Buffalo
+
+
+    public sauceType currentSauce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PickSauce(int sauceChoice)
+    {
+        switch(sauceChoice)
+        {
+            case 1:
+            currentSauce = sauceType.HoneyGarlic;
+            break;
+            case 2:
+            currentSauce = sauceType.BBQ;
+            break;
+            case 3:
+            currentSauce = sauceType.Buffalo;
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
