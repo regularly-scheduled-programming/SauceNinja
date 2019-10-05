7197bdfa0eeea50dee240f6cff2d774e21c4e319
NeilHansen <neil.hansen17@gmail.com>
2019-10-05 17:05:44 -0400

    ARTED UP!!!

--- SauceNinjaGame/sauceNinja/Assets/Scripts/SauceTypePicker.cs
+++ SauceNinjaGame/sauceNinja/Assets/Scripts/SauceTypePicker.cs
@@ -10,13 +10,13 @@ public class SauceTypePicker :  WingType
     
 
     public GameObject sauceButton1;
-    public Color hg;
+  
    
     public GameObject sauceButton2;
-    public Color bbq;
+  
 
     public GameObject sauceButton3;
-    public Color buff;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -30,30 +30,22 @@ public class SauceTypePicker :  WingType
         {
              case 0:
             currentBrushSauce = sauceType.NONE;
-            sauceButton1.GetComponent<Image>().color = hg;
-            sauceButton2.GetComponent<Image>().color = bbq;
-            sauceButton3.GetComponent<Image>().color = buff;
+         
             break;
 
             case 1:
             currentBrushSauce = sauceType.HoneyGarlic;
-            sauceButton1.GetComponent<Image>().color = Color.green;
-            sauceButton2.GetComponent<Image>().color =bbq;
-            sauceButton3.GetComponent<Image>().color = buff;
+           
             break;
 
             case 2:
             currentBrushSauce = sauceType.BBQ;
-            sauceButton1.GetComponent<Image>().color = hg;
-            sauceButton2.GetComponent<Image>().color = Color.green;
-            sauceButton3.GetComponent<Image>().color = buff;
+            
             break;
 
             case 3:
             currentBrushSauce = sauceType.Buffalo;
-            sauceButton1.GetComponent<Image>().color = hg;
-            sauceButton2.GetComponent<Image>().color = bbq;
-            sauceButton3.GetComponent<Image>().color = Color.green;
+           
             break;
         }
     }
